cb68a0eb0c77ed2ddbd31616b2681de0484c1dfd
Christee Song <christee.song@gmail.com>
2019-08-05 15:12:02 +0900

    fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
+  # Generic syntax:
+  # verb 'path', to: 'controller#action', as: :route_name
+
+  #   Prefix Verb URI Pattern       Controller#Action
+  #    ask GET  /ask(.:format)    questions#ask
+  # answer GET  /answer(.:format) questions#answer
 end
